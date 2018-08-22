2294b0ca94309ff7597e04a5627e2773bc7a68b1
曾晓龙大大 <zengxiaolong430722@163.com>
2018-08-22 09:17:31 +0800

    测试界面

--- FD.Tiny.UI.Controls.Demo/App_Start/RouteConfig.cs
+++ FD.Tiny.UI.Controls.Demo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FD.Tiny.UI.Controls.Demo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DynamicForm", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
