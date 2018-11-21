f8aef8f347d6233b0424089d875251f8c9d29d4d
doku <29413606@qq.com>
2018-11-21 11:21:52 +0800

    add cache

---
+++ FD.Tiny.Common.Utility/Cache/ICacheContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FD.Tiny.Common.Utility.Cache
+{
+    public abstract class ICacheContext
+    {
+        /// <summary>
+        /// 获取缓存项
+        /// </summary>
+        /// <typeparam name="T">缓存对象类型</typeparam>
+        /// <param name="key">键</param>
+        /// <returns>缓存对象</returns>
+        public abstract T Get<T>(string key);
+
+        /// <summary>
+        /// 设置缓存项
+        /// </summary>
+        /// <typeparam name="T">缓存对象类型</typeparam>
+        /// <param name="key">键</param>
+        /// <param name="t">缓存对象</param>
+        /// <returns>true成功,false失败</returns>
+        public abstract bool Set<T>(string key, T t, DateTime expire);
+
+        /// <summary>
+        /// 移除一个缓存项
+        /// </summary>
+        /// <param name="key">缓存项key</param>
+        /// <returns>true成功,false失败</returns>
+        public abstract bool Remove(string key);
+
+    }
+}
---
+++ FD.Tiny.Common.Utility/Cache/MemoryCacheHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace FD.Tiny.Common.Utility.Cache
+{
+    /// <summary>
+    /// 基于MemoryCache的缓存辅助类
+    /// </summary>
+    public static class MemoryCacheHelper
+    {
+        private static readonly Object _locker = new object();
+
+        public static T GetCacheItem<T>(String key, Func<T> cachePopulate, CacheEntryRemovedCallback removedCallback, TimeSpan? slidingExpiration = null, DateTime? absoluteExpiration = null)
+        {
+            if (String.IsNullOrWhiteSpace(key)) throw new ArgumentException("Invalid cache key");
+            if (cachePopulate == null) throw new ArgumentNullException("cachePopulate");
+            if (slidingExpiration == null && absoluteExpiration == null) throw new ArgumentException("Either a sliding expiration or absolute must be provided");
+
+            if (MemoryCache.Default[key] == null)
+            {
+                lock (_locker)
+                {
+                    if (MemoryCache.Default[key] == null)
+                    {
+                        var item = new CacheItem(key, cachePopulate());
+                        var policy = CreatePolicy(slidingExpiration, absoluteExpiration, removedCallback);
+
+                        MemoryCache.Default.Add(item, policy);
+                    }
+                }
+            }
+
+            return (T)MemoryCache.Default[key];
+        }
+
+        private static CacheItemPolicy CreatePolicy(TimeSpan? slidingExpiration, DateTime? absoluteExpiration, CacheEntryRemovedCallback removedCallback)
+        {
+            var policy = new CacheItemPolicy();
+
+            if (absoluteExpiration.HasValue)
+            {
+                policy.AbsoluteExpiration = absoluteExpiration.Value;
+            }
+            else if (slidingExpiration.HasValue)
+            {
+                policy.SlidingExpiration = slidingExpiration.Value;
+            }
+
+            policy.Priority = CacheItemPriority.Default;
+            if (removedCallback != null)
+                policy.RemovedCallback = removedCallback;
+
+            return policy;
+        }
+        public static bool Contains(string key)
+        {
+            return MemoryCache.Default.Contains(key);
+        }
+
+        public static void RemoveCache(string key)
+        {
+            MemoryCache.Default.Remove(key);
+        }
+    }
+}
